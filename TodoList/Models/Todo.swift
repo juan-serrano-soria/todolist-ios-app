19cd5e75810d5dcde7567c8876a9d659af4b70de
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2025-03-06 22:32:00 +0100

    Fix build issue + add Todo model

---
+++ TodoList/Models/Todo.swift
@@ -0,0 +1,20 @@
+//
+//  Todo.swift
+//  TodoList
+//
+//  Created by Serrano Soria, Juan on 06/03/2025.
+//
+
+import Foundation
+
+struct Todo {
+    let id: UUID
+    var title: String
+    var isCompleted: Bool
+    
+    init(title: String, isCompleted: Bool = false) {
+        self.id = UUID()
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+}
