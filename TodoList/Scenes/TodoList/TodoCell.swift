31f089b2c72cd34b5a194eae520b9fd104b6829a
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2025-03-07 16:42:22 +0100

    Add basic cell and list view controller

---
+++ TodoList/Scenes/TodoList/TodoCell.swift
@@ -0,0 +1,20 @@
+//
+//  TodoCell.swift
+//  TodoList
+//
+//  Created by Serrano Soria, Juan on 07/03/2025.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
--- TodoList/Scenes/TodoList/TodoListViewController.swift
+++ TodoList/Scenes/TodoList/TodoListViewController.swift
@@ -8,12 +8,80 @@
 import UIKit
 
 class TodoListViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    private var todos: [Todo] = []
+    
+    
+    // MARK: - UI Elements
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(TodoCell.self, forCellReuseIdentifier: "Todocell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        setupNavigationBar()
+    }
+    
+    // MARK: - Setup Methods
+    private func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func setupNavigationBar() {
+        title = "Todo List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addButtonTapped)
+        )
+        
+        navigationItem.rightBarButtonItem = addButton
+    }
+    
+    // MARK: - Actions
+    @objc private func addButtonTapped() {
+        // handle adding todos
     }
 
+}
 
+// MARK: - UITableView DataSource and Delegate
+extension TodoListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    // Configure and return cell for each row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        let todo = todos[indexPath.row]
+        cell.textLabel?.text = todo.title
+        cell.accessoryType = todo.isCompleted ? .checkmark : .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // add todo completion
+    }
+    
 }
 
